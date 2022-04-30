09623a1bbad95b26b8b52d61353c57ebf38f5dcf
Denisa Plačková <dejna.plackova@gmail.com>
2022-04-30 11:47:46 +0200

    srolování navigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './style.css';
 
 console.log('funguju!');
+
+const menuBtn = document.querySelector('#nav-btn');
+const navigace = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () => {
+  navigace.classList.toggle('nav-closed');
+});
+
+const allNavigace = document.querySelectorAll('nav a');
+
+allNavigace.forEach((item) =>
+  item.addEventListener('click', () => {
+    navigace.classList.toggle('nav-closed');
+  }),
+);
