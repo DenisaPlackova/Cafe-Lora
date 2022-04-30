9c72b55dc7b3d9c1b13a6a7b890f6e7c38e418b5
Denisa Plačková <dejna.plackova@gmail.com>
2022-04-30 12:33:51 +0200

    Nastavení objednávání

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 
 console.log('funguju!');
 
+/* schovávání navigae*/
 const menuBtn = document.querySelector('#nav-btn');
 const navigace = document.querySelector('nav');
 
@@ -13,6 +14,24 @@ const allNavigace = document.querySelectorAll('nav a');
 
 allNavigace.forEach((item) =>
   item.addEventListener('click', () => {
-    navigace.classList.toggle('nav-closed');
+    navigace.classList.add('nav-closed');
   }),
 );
+
+/*objednávání*/
+
+const objednavka = document.querySelector('.order-btn');
+const drinkCup = document.querySelector('.drink__cup');
+
+let ordered = false;
+objednavka.addEventListener('click', () => {
+  if (!ordered) {
+    objednavka.textContent = 'Zrušit';
+    drinkCup.classList.add('drink__cup--selected');
+    ordered = true;
+  } else {
+    objednavka.textContent = 'Objednat';
+    drinkCup.classList.remove('drink__cup--selected');
+    ordered = false;
+  }
+});
